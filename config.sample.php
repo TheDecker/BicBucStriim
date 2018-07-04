48fe3895686d82ba0e0011326dcc65c94b239bff
Martin Ried <MartinRied@gmx.net>
2018-07-04 13:00:33 +0200

    Cleanup

--- config.sample.php
+++ config.sample.php
@@ -1,40 +1,4 @@
-# Allowed languages, i.e. languages with translations
-$allowedLangs = array('de', 'en', 'es', 'fr', 'gl' , 'hu', 'it', 'nl');
-# Fallback language if the browser prefers otherau than the allowed languages
 $fallbackLang = 'en';
-# Application Name
 $appname = 'BicBucStriim';
-# App version
-$appversion = '1.4.1-SNAPSHOT';
-
 $appmode = 'development';
-
-# Init app globals
-$globalSettings = array();
-$globalSettings['appname'] = $appname;
-$globalSettings['version'] = $appversion;
-$globalSettings['sep'] = ' :: ';
-# Find the user language, either one of the allowed languages or
-# English as a fallback.
-$globalSettings['lang'] = getUserLang($allowedLangs, $fallbackLang);
-$globalSettings['l10n'] = new L10n($globalSettings['lang']);
-$globalSettings['langa'] = $globalSettings['l10n']->langa;
-$globalSettings['langb'] = $globalSettings['l10n']->langb;
-# Init admin settings with std values, for upgrades or db errors
-$globalSettings[CALIBRE_DIR] = '';
-$globalSettings[DB_VERSION] = DB_SCHEMA_VERSION;
-$globalSettings[KINDLE] = 0;
-$globalSettings[KINDLE_FROM_EMAIL] = '';
-$globalSettings[THUMB_GEN_CLIPPED] = 1;
-$globalSettings[PAGE_SIZE] = 30;
-$globalSettings[DISPLAY_APP_NAME] = $appname;
-$globalSettings[MAILER] = Mailer::MAIL;
-$globalSettings[SMTP_USER] = '';
-$globalSettings[SMTP_PASSWORD] = '';
-$globalSettings[SMTP_SERVER] = '';
-$globalSettings[SMTP_PORT] = 25;
-$globalSettings[SMTP_ENCRYPTION] = 0;
-$globalSettings[METADATA_UPDATE] = 0;
-$globalSettings[LOGIN_REQUIRED] = 1;
-$globalSettings[TITLE_TIME_SORT] = TITLE_TIME_SORT_TIMESTAMP;
-$globalSettings[RELATIVE_URLS] = 1;
+$appdb = 'data/data.db';
--- index.php
+++ index.php
@@ -27,8 +27,17 @@ require_once 'metadata_epub.php';
 use dflydev\markdown\MarkdownExtraParser;
 use Aura\Auth as Auth;
 
-require 'config.php';
-    
+# Allowed languages, i.e. languages with translations
+$allowedLangs = array('de', 'en', 'es', 'fr', 'gl' , 'hu', 'it', 'nl');
+# Fallback language if the browser prefers otherau than the allowed languages
+$fallbackLang = 'en';
+# Application Name
+$appname = 'BicBucStriim';
+# App version
+$appversion = '1.4.2a';
+
+require_once ('config.php');
+
 # Init app and routes
 $app = new \Slim\Slim(array(
     'view' => new \Slim\Views\Twig(),
@@ -92,13 +101,43 @@ function confdebug()
     error_reporting(E_ALL | E_STRICT);
 }
 
+# Init app globals
+$globalSettings = array();
+$globalSettings['appname'] = $appname;
+$globalSettings['version'] = $appversion;
+$globalSettings['sep'] = ' :: ';
+# Find the user language, either one of the allowed languages or
+# English as a fallback.
+$globalSettings['lang'] = getUserLang($allowedLangs, $fallbackLang);
+$globalSettings['l10n'] = new L10n($globalSettings['lang']);
+$globalSettings['langa'] = $globalSettings['l10n']->langa;
+$globalSettings['langb'] = $globalSettings['l10n']->langb;
+# Init admin settings with std values, for upgrades or db errors
+$globalSettings[CALIBRE_DIR] = '';
+$globalSettings[DB_VERSION] = DB_SCHEMA_VERSION;
+$globalSettings[KINDLE] = 0;
+$globalSettings[KINDLE_FROM_EMAIL] = '';
+$globalSettings[THUMB_GEN_CLIPPED] = 1;
+$globalSettings[PAGE_SIZE] = 30;
+$globalSettings[DISPLAY_APP_NAME] = $appname;
+$globalSettings[MAILER] = Mailer::MAIL;
+$globalSettings[SMTP_USER] = '';
+$globalSettings[SMTP_PASSWORD] = '';
+$globalSettings[SMTP_SERVER] = '';
+$globalSettings[SMTP_PORT] = 25;
+$globalSettings[SMTP_ENCRYPTION] = 0;
+$globalSettings[METADATA_UPDATE] = 0;
+$globalSettings[LOGIN_REQUIRED] = 1;
+$globalSettings[TITLE_TIME_SORT] = TITLE_TIME_SORT_TIMESTAMP;
+$globalSettings[RELATIVE_URLS] = 1;
+
 $knownConfigs = array(CALIBRE_DIR, DB_VERSION, KINDLE, KINDLE_FROM_EMAIL,
     THUMB_GEN_CLIPPED, PAGE_SIZE, DISPLAY_APP_NAME, MAILER, SMTP_SERVER,
     SMTP_PORT, SMTP_USER, SMTP_PASSWORD, SMTP_ENCRYPTION, METADATA_UPDATE,
     LOGIN_REQUIRED, TITLE_TIME_SORT, RELATIVE_URLS);
 
 # Freeze (true) DB schema before release! Set to false for DB development.
-$app->bbs = new BicBucStriim('data/data.db', true);
+$app->bbs = new BicBucStriim($appdb, true);
 $app->add(new \CalibreConfigMiddleware(CALIBRE_DIR));
 $app->add(new \LoginMiddleware($appname, array('js', 'img', 'style')));
 $app->add(new \OwnConfigMiddleware($knownConfigs));
